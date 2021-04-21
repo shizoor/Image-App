70a8981aa3e118c867493dd57f6608041ad1701c
Richard Ferris <richard.ferris@gmail.com>
2021-04-21 22:13:56 +0100

    Basic image app first upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Image_App_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blurToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(629, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.blurToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // blurToolStripMenuItem
+            // 
+            this.blurToolStripMenuItem.Name = "blurToolStripMenuItem";
+            this.blurToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.blurToolStripMenuItem.Text = "Blur";
+            this.blurToolStripMenuItem.Click += new System.EventHandler(this.blurToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(629, 351);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(619, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 379);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Image manipulation program";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blurToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Image_App_2
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Basic image loading app \n Shizoor \n 2021");
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.InitialDirectory = "%APPDATA%";
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Filter = "png files (*.png)|*.png|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    filePath = openFileDialog.FileName;
+
+                    //pictureBox1.ImageLocation = filePath;  One way of doing it
+                    pictureBox1.Image = Bitmap.FromFile(filePath);   /// another
+                    
+                  
+                }
+            }
+        }
+
+        private void blurToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Rectangle bitmapshape = new System.Drawing.Rectangle();
+            int x, y;
+            //Int32 pixelcolour;
+            float r, g, b;
+            Int32 pixel;
+
+            bitmapshape.X = 0;
+            bitmapshape.Y = 0;
+
+
+            //Scrape the image into an array if it's not null
+            if (pictureBox1.Image != null)
+            {
+                Bitmap image1 = new Bitmap(pictureBox1.Image);
+                Bitmap image2 = new Bitmap(pictureBox1.Image);
+
+                bitmapshape.Width = image1.Width;
+                bitmapshape.Height = image1.Height;
+
+                byte[,,] pixelarray = new byte[image1.Width, image1.Height, 3];   //red, green, blue
+                byte[,,] pixelarray2 = new byte[image1.Width, image1.Height, 3];   //buffer for applying an effect
+                label1.Text = ("Blur called");
+                for (x = 0; x < image1.Width; x++)
+                {
+                    for (y = 0; y < image1.Height; y++)
+                    {
+                        pixel = image1.GetPixel(x, y).ToArgb();
+                        pixelarray[x, y, 0] = (byte)((pixel & 0xFF0000) >> 16);
+                        pixelarray[x, y, 1] = (byte)((pixel & 0xFF00) >> 8);
+                        pixelarray[x, y, 2] = (byte)((pixel & 0xFF));
+                    }
+                }
+
+                //Blur the image
+
+                for (x = 1; x < (image1.Width - 1); x++)
+                {
+                    for (y = 1; y < image1.Height - 1; y++)
+                    {
+                        r = (pixelarray[x, y, 0] + pixelarray[x - 1, y, 0] + pixelarray[x + 1, y, 0] + pixelarray[x, y - 1, 0] + pixelarray[x, y + 1, 0]) / 5;
+                        g = (pixelarray[x, y, 1] + pixelarray[x - 1, y, 1] + pixelarray[x + 1, y, 1] + pixelarray[x, y - 1, 1] + pixelarray[x, y + 1, 1]) / 5;
+                        b = (pixelarray[x, y, 2] + pixelarray[x - 1, y, 2] + pixelarray[x + 1, y, 2] + pixelarray[x, y - 1, 2] + pixelarray[x, y + 1, 2]) / 5;
+
+                        pixelarray2[x, y, 0] = (byte)r;
+                        pixelarray2[x, y, 1] = (byte)g;
+                        pixelarray2[x, y, 2] = (byte)b;
+
+                    }
+                }
+
+                for (x = 0; x < image1.Width; x++)
+                {
+                    for (y = 0; y < image1.Height; y++)
+                    {
+                        //pixelcolour = pixelarray2[x, y, 0] + pixelarray2[x, y, 1] >> 8 + pixelarray2[x, y, 2] >> 16;
+                        image2.SetPixel(x, y, Color.FromArgb(255, pixelarray2[x, y, 0], pixelarray2[x, y, 1], pixelarray2[x, y, 2]));
+                        //image2.SetPixel(x, y, Color.FromArgb(pixelcolour));
+                        //textBox1.Text = ("Chewing pixel" + x + "," + y);
+                    }
+                }
+
+                image1 = image2.Clone(bitmapshape, System.Drawing.Imaging.PixelFormat.DontCare);   // meow
+
+                pictureBox1.Image = image1;
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Please load a picture first.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                dialog.Filter= "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|png image|*.png";
+                dialog.Title = "Save the image";
+                dialog.ShowDialog();
+
+                bmp.Save(dialog.FileName);
+                label1.Text = "Image saved";
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Please load a picture first.");
+            }
+        }
+    }
+}
